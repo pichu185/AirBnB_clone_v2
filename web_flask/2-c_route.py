7bede47b6536e85c4352977da41ffef7d49c375e
Pichu185 <pichu_otegui@hotmail.comm>
2021-09-12 17:36:03 -0300

    2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python3
-"""starts flask web app"""
+"""Write a script that starts a Flask web application"""
 
+from typing import Text
 from flask import Flask
 app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """display “Hello HBNB!”"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+	"""display “HBNB”"""
+	return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """displays 'C' followed by the value of <text>"""
+    text = text.replace("_", " ")
+	return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
