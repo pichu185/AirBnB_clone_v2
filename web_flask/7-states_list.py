40eb95fefde96f5abed63daea97943c6d423885a
Pichu185 <pichu_otegui@hotmail.comm>
2021-09-12 19:34:00 -0300

    7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""starts flask web app"""
+"""Write a script that starts a Flask web application"""
 
 from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
 app = Flask(__name__)
