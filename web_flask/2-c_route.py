94ce6052e5733bbefd7c2deb6e50ccb1b004c17e
Pichu185 <pichu_otegui@hotmail.comm>
2021-09-12 17:55:51 -0300

    2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -13,16 +13,15 @@ def home():
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	"""display “HBNB”"""
-	return ("HBNB")
+    """display “HBNB”"""
+    return ("HBNB")
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """displays 'C' followed by the value of <text>"""
     text = text.replace("_", " ")
-	return ("C {}".format(text))
-
+    return ("C {}".format(text))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
