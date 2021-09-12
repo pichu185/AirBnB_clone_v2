e4f3797063874e4611ea1e3c773e441f220fc44c
Pichu185 <pichu_otegui@hotmail.comm>
2021-09-12 19:12:32 -0300

    4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -27,15 +27,16 @@ def c(text):
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
-    """displays "Python", followed by the value of the <text>"""
+    """displays 'Python', followed by the value of the <text>"""
     text = text.replace("_", " ")
     return ("Python {}".format(text))
 
 
-@app.route("/number/<int: n >", strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """display “n is a number” only if n is an integer"""
     return ("{} is a number".format(n))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
