80e01f851fddb1dd7ec2098ef6caa5cb555fbd95
Pichu185 <pichu_otegui@hotmail.comm>
2021-09-12 20:05:02 -0300

    8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
-"""starts flask web app"""
+"""Write a script that starts a Flask web application"""
 
 from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def remove_sesh(self):
+    """After each request you must remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """render html page with a list of states"""
+    states = storage.all(State)
+    return (render_template('7-states_list.html', states=states))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
