ffbbb7ad7cf2b5c6258a99c41a01da0b004b5c4b
Pichu185 <pichu_otegui@hotmail.comm>
2021-09-12 20:01:57 -0300

    7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,3 +7,20 @@ from models import storage
 from models.state import State
 
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def remove_sesh(self):
+    """After each request you must remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """render html page with a list of states"""
+    states = storage.all(State)
+    return (render_template('7-states_list.html', states=states))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
