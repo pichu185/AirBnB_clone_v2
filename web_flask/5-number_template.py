8c7c763e3d8405a18c554f399fef75414c472d3a
Pichu185 <pichu_otegui@hotmail.comm>
2021-09-12 19:00:08 -0300

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,13 +33,13 @@ def python(text="is cool"):
     return ("Python {}".format(text))
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """display “n is a number” only if n is an integer"""
     return ("{} is a number".format(n))
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route('/ number_template/<int: n >', strict_slashes=False)
 def template(n):
     """display a HTML page only if n is an integer"""
     return (render_template("5-number.html", numb=n)
